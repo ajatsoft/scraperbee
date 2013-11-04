a73e0f3e89d9991c20aaa75bbeba97148076d1cf
lisa ballard <lballard.cat@gmail.com>
2013-11-04 01:00:47 -0800

    adds url conf and heroku postgres setting

--- scraperbee/urls.py
+++ scraperbee/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.defaults import *
+from .api import EntryResource
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
+entry_resource = EntryResource()
+
 urlpatterns = patterns('',
+    (r'^entry/', include(entry_resource.urls)),
+
     # Examples:
     # url(r'^$', 'scraperbee.views.home', name='home'),
     # url(r'^scraperbee/', include('scraperbee.foo.urls')),
